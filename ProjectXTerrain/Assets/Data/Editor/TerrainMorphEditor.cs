62c6429fd17a4f76fb372a8355131b0d5b39ae23
NekIP <Shestakov.rm@list.ru>
2017-08-20 15:25:44 +0300

    Added editor scripts

---
+++ ProjectXTerrain/Assets/Data/Editor/TerrainMorphEditor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(TerrainMorph))]
+public class TerrainMorphEditor : Editor
+{
+    TerrainMorph terrain;
+
+    void OnEnable()
+    {
+        terrain = (TerrainMorph)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        if (!terrain.WasInitialized)
+        {
+            if (GUILayout.Button("Initialize"))
+            {
+                terrain.Initialize();
+            }
+        }
+    }
+}
